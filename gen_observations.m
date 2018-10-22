9b0b9b97edd4651b21798222ebad27f1abdc3e22
JaeyoungLim <gokoreas@gmail.com>
2018-10-22 14:13:04 +0200

    package creating submaps into a subfunction

--- gen_observations.m
+++ gen_observations.m
@@ -27,40 +27,14 @@ if options.plotting
     plot_binmap(binmap_true, path);
 end
 
-for j = 1:size(path, 1)
-    mavPose = [path(j, :), 0];
+for j = 1:size(path, 1)-1
+    position = path(j, :);
+    velocity = path(j +1, :) - path(j, :);
+    ram = atan2(velocity(2), velocity(1));
+    
+    mavPose = [position, ram];
     % Create a partial map based on observation
-    map_obs = robotics.OccupancyGrid(width_m, height_m, resolution_m);
-
-    %% Generate a local map
-
-    angles = (mavPose(3)-fov/2):0.01:(mavPose(3)+fov/2);
-    intsectionPts = rayIntersection(map_true,mavPose,angles,maxrange);
-
-    for i = 1:1:size(intsectionPts, 1)
-
-        ray_endpoint = intsectionPts(i, :);
-        if norm(double(isnan(ray_endpoint))) > 0
-            ray_endpoint = mavPose(1:2) + [maxrange*cos(angles(i)), maxrange*sin(angles(i))]; 
-            ray_endpoint(1) = max(min(ray_endpoint(1), map_obs.XWorldLimits(2)), map_obs.XWorldLimits(1));
-            ray_endpoint(2) = max(min(ray_endpoint(2), map_obs.YWorldLimits(2)), map_obs.YWorldLimits(1));
-
-            [endpoints, midpoints] = raycast(map_obs, mavPose(1:2), ray_endpoint);
-
-            setOccupancy(map_obs, midpoints, zeros(size(midpoints, 1), 1), 'grid');
-        else
-            ray_endpoint(1) = max(min(ray_endpoint(1), map_obs.XWorldLimits(2)), map_obs.XWorldLimits(1));
-            ray_endpoint(2) = max(min(ray_endpoint(2), map_obs.YWorldLimits(2)), map_obs.YWorldLimits(1));
-
-            [endpoints, midpoints] = raycast(map_obs, mavPose(1:2), ray_endpoint);
-
-            setOccupancy(map_obs, midpoints, zeros(size(midpoints, 1), 1), 'grid');
-            setOccupancy(map_obs, endpoints, ones(size(endpoints, 1), 1), 'grid'); 
-        end
-    end
-
-    %% Mark unknown space / known space
-
+    map_obs = get_localmap(map_true, mavPose);
 
     %% Plot
     if options.plotting
