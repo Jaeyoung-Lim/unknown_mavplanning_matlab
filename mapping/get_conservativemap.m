171bfe4c498b6d68a4a36f43d87f21fe5bdb4c0e
JaeyoungLim <gokoreas@gmail.com>
2018-11-02 15:21:40 +0100

    Parse local goals from global path

---
+++ mapping/get_conservativemap.m
@@ -0,0 +1,8 @@
+function [map_conservative] = get_conservativemap(map, param, pose)
+    %% Initialize
+    % Returns optimistic map from 
+    
+    map_conservative = robotics.OccupancyGrid(double(map.occupancyMatrix), param.globalmap.resolution);
+    map_conservative = robotics.BinaryOccupancyGrid(map_conservative.occupancyMatrix < map_conservative.FreeThreshold);
+    
+end
--- navigate.m
+++ navigate.m
@@ -1,4 +1,9 @@
 function navigate(params, videoObj)
+%% Parameters
+plan_horizon = 10;
+update_rate = 2;
+dt = 0.1;
+
 %% Generate Global Trajectory
 
 switch params.map_type
@@ -18,45 +23,42 @@ setOccupancy(binmap_true, vertcat(params.start_point, params.goal_point, ...
 global_start = params.start_point;
 global_goal = params.goal_point;
 
-planning_horizon = 10;
-update_rate = 2;
-dt = 0.1;
-
-
 %% Plan Optimistic global trajectory 
 mavPose = [global_start, 0.0];
 localmap_obs = robotics.OccupancyGrid(params.globalmap.width, params.globalmap.height, params.globalmap.resolution);
 map_partial = get_localmap('increment', binmap_true, localmap_obs, params, mavPose);
 opt_binmap = get_optimisticmap(map_partial, params, mavPose);
+cons_binmap = get_conservativemap(map_partial, params, mavPose);
 
-[T, globalpath] = plan_trajectory('polynomial', opt_binmap, global_start, global_goal);
+[T, globalpath] = plan_trajectory('chomp', opt_binmap, global_start, global_goal);
 
-%% Initialize local plan
-local_start = start_pos;
-% local_goal = ;
+curr_pos = mavPose(1:2);
 
-
+%% Parse path from global path
+local_start = globalpath(sum(T < update_rate), :);
+local_goal = goalfrompath(cons_binmap, globalpath, curr_pos, plan_horizon);
+ 
 while true
-    %% TODO: Parse path from global path
-    local_start = start_pos;
-    local_goal = global_goal;
-
-
-    localpath = globalpath;
-    %% Plan Local trajectory
-    [T, globalpath] = plan_trajectory(params.planner, binmap_true, local_start, local_goal);
-
+    
+    local_start = globalpath(sum(T < update_rate), :);
+    local_goal = globalpath(sum(vecnorm(globalpath - curr_pos, 2, 2) < plan_horizon), :);
+    
+    %% Check if we can see the goal
+    
+    %% Generate Local map
+    % Create a partial map based on observation
+    [localmap_obs, localmap_full] = get_localmap(binmap_true, localmap_obs, params, mavPose);
         
+    %% Replan Local trajectory
+    [T, localpath] = plan_trajectory('chomp', localmap_full, local_start, local_goal);
+
+    %% Move along local trajectory
     for t = 1:dt:update_rate
         position = path(j, :);
         velocity = path(j +1, :) - path(j, :);
         ram = atan2(velocity(2), velocity(1));
 
         mavPose = [position, ram];
-        %% Generate Local map
-        % Create a partial map based on observation
-        [localmap_obs, localmap_full] = get_localmap(binmap_true, localmap_obs, params, mavPose);
-        
         %% Plot
         if params.visualization
             plot_summary(params, binmap_true, localmap_obs, path, mavPose, videoObj);
@@ -66,6 +68,10 @@ while true
         end
     end
     start_pos = mavPose(1:2);
+
+    local_start = globalpath(sum(T < update_rate), :);
+    local_goal = global_goal;
+
 end
 
 end
@@ -74,4 +80,16 @@ function [isgoal] = goalreached(current_position, goal_position)
 
     tolerance = 0.1;
     isgoal = norm(current_position - goal_position) <  tolerance;
+end
+
+function goal = goalfrompath(map, path, current_position, horizon)
+    idx = sum(vecnorm(path - current_position, 2, 2) < horizon);
+    
+    while true
+        if map.getOccupancy(path(idx, :))
+           break;
+        end
+        idx = idx -1;
+    end
+    goal = path(idx, :);
 end
