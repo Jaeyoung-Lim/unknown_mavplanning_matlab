4a6389d89a5e3b8bea96f3bf5f627cfccacc847a
JaeyoungLim <gokoreas@gmail.com>
2018-11-02 17:21:12 +0100

    inflate occupancy gridmap for planning for mav size

--- planners/plan_trajectory.m
+++ planners/plan_trajectory.m
@@ -1,6 +1,8 @@
 function [T, path]=plan_trajectory(planner_type, binary_occupancygrid, start_position, goal_position)
 %% Run Planner between start and endpoint depending on the planner type
 
+binary_occupancygrid.inflate(0.5);
+
 switch planner_type
     case 'a_star'
         path = a_star(binary_occupancygrid, start_position, goal_position);
@@ -10,6 +12,7 @@ switch planner_type
         [T, path] = sample_trajectory(trajectory, 0.1);
         
     case 'chomp'
+
         trajectory = continous_chomp(binary_occupancygrid, start_position, goal_position);
         [T, path] = sample_trajectory(trajectory, 0.1);
 end
