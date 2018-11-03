bcb6b7a180539ef11d17928d7912842883d552b1
JaeyoungLim <gokoreas@gmail.com>
2018-11-03 09:23:11 +0100

    Fix conservative optimistic map conversion

--- mapping/get_optimisticmap.m
+++ mapping/get_optimisticmap.m
@@ -3,6 +3,6 @@ function [map_optimistic] = get_optimisticmap(map, param, pose)
     % Returns optimistic map from 
     
     map_optimistic = robotics.OccupancyGrid(double(map.occupancyMatrix), param.globalmap.resolution);
-    map_optimistic = robotics.BinaryOccupancyGrid(map_optimistic.occupancyMatrix > map_optimistic.FreeThreshold);
+    map_optimistic = robotics.BinaryOccupancyGrid(map_optimistic.occupancyMatrix > map_optimistic.OccupiedThreshold);
     
 end
