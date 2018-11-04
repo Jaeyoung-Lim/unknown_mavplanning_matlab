26b4d396cc1a305581287cd48d8bffd12409f661
JaeyoungLim <gokoreas@gmail.com>
2018-11-04 20:12:04 +0100

    Fix local goal selection bug

--- trajectory/getLocalGoal.m
+++ trajectory/getLocalGoal.m
@@ -1,7 +1,7 @@
 function local_goal = getLocalGoal(occupancy_map, curr_pose, path, goal, plan_horizon)
 
-if occupancy_map.getOccupancy(goal)
-    % Local goal is global goal if global goal is visible
+if ~occupancy_map.getOccupancy(goal)
+    % Local goal is global goal if global goal is free
     local_goal = goal;
 else
     local_goal = goalfrompath(occupancy_map, path, curr_pose(1:2), plan_horizon);
