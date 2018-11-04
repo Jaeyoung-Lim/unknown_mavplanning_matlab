c0863873d92e3567767f2b4b59e32a88d06a175b
JaeyoungLim <gokoreas@gmail.com>
2018-11-04 19:34:08 +0100

    Fixes on global planner

--- navigate.m
+++ navigate.m
@@ -5,38 +5,32 @@ update_rate = 10;
 dt = 0.1;
 
 %% Plan Optimistic global trajectory 
-
-% Set gloabl start and goal position
-global_start = params.start_point;
+global_start = params.start_point; % Set gloabl start and goal position
 global_goal = params.goal_point;
+mavPose = [global_start, 0.0]; % Current position starts from global start
 
-mavPose = [global_start, 0.0];
+% Initialize Local map
 localmap_obs = robotics.OccupancyGrid(params.globalmap.width, params.globalmap.height, params.globalmap.resolution);
-map_partial = get_localmap('increment', binmap_true, localmap_obs, params, mavPose);
-opt_binmap = get_optimisticmap(map_partial, params, mavPose);
-cons_binmap = get_conservativemap(map_partial, params, mavPose);
+
+map_partial = get_localmap('increment', binmap_true, localmap_obs, params, mavPose); % 
+opt_binmap = get_optimisticmap(map_partial, params, mavPose); % Optimistic binary occupancy grid
 
 % Plan global trajectory
-[T, globalpath] = plan_trajectory('polynomial', opt_binmap, global_start, global_goal);
+[~, globalpath] = plan_trajectory('polynomial', opt_binmap, global_start, global_goal);
 
 % Parse intermediate goal from global path
-local_start = globalpath(sum(T < update_rate), :);
-if cons_binmap.getOccupancy(global_goal)
-    local_goal = global_goal;
-else
-    local_goal = goalfrompath(cons_binmap, globalpath, mavPose(1:2), plan_horizon);
-end
+% local_start = globalpath(sum(T < update_rate), :);
 
 %% Local replanning from global path
 
 while true        
-    % Create a partial map based on observation
-    [localmap_obs, ~] = get_localmap('increment', binmap_true, localmap_obs, params, mavPose);
-    
     % Replan Local trajectory from trajectory replanning rate
-    local_start = mavPose(1:2);
-    
+    [localmap_obs, ~] = get_localmap('increment', binmap_true, localmap_obs, params, mavPose);     % Create a partial map based on observation
     cons_binmap = get_conservativemap(localmap_obs, params, mavPose);
+
+    local_start = mavPose(1:2);
+    local_goal = getLocalGoal(cons_binmap, mavPose, globalpath, global_goal, plan_horizon); % Parse intermediate goal from global path
+
     [localT, localpath] = plan_trajectory('chomp', cons_binmap, local_start, local_goal);
 
     %% Move along local trajectory
@@ -46,18 +40,11 @@ while true
         if params.visualization
             plot_summary(params, binmap_true, localmap_obs, localpath, globalpath, mavPose, videoObj); % Plot MAV moving in environment
         end
+        if goalreached(mavPose(1:2), global_goal)
+            disp('Goal Reached!');
+            break;
+        end
     end
-    
-    % Check if goal is visible
-    if cons_binmap.getOccupancy(global_goal)
-        local_goal = global_goal;
-    else
-        local_goal = goalfrompath(cons_binmap, globalpath, mavPose(1:2), plan_horizon);
-    end
-    
-    if goalreached(mavPose(1:2), global_goal)
-        break;
-    end
-
 end
+
 end
