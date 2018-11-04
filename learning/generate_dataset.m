9142b77a2acc5f7b85299b2bdc263f441655e768
JaeyoungLim <gokoreas@gmail.com>
2018-11-04 02:38:02 +0100

    Generate dataset

---
+++ learning/generate_dataset.m
@@ -0,0 +1,12 @@
+%% Parameters
+directory = '/home/jalim/dev/unknown_mavplanning_matlab/data';
+num_maps = 200;
+
+%% Generate and save maps
+param = Param_LOCALMAP;
+
+for j=1:num_maps
+    map = generate_environment(param);
+    ffilepath = strcat(directory ,'/fullmap/', int2str(j),'.jpg');
+    imwrite(1-map.occupancyMatrix, ffilepath);
+end
