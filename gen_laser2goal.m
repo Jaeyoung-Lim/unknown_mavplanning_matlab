7265b3e08e4eba89539d1445f237d221802e7d28
JaeyoungLim <gokoreas@gmail.com>
2018-11-06 20:06:33 +0100

    generate data using laser range measurements

--- gen_laser2goal.m
+++ gen_laser2goal.m
@@ -1,5 +1,7 @@
 function gen_laser2goal(params, map, videoObj)
 %% Make a plan using A*.
+X = []; % Input states for learning
+T = []; % Target states
 
 global_start = params.start_point;
 global_goal = params.goal_point;
@@ -13,14 +15,19 @@ for i=1:numel(mavT)
     t = mavT(i);
     mavPose = posefromtrajectoy(mavPath, mavT, t);
     
-    %% TODO: Get local observations
-%     data = generateData(mavPose, map)
+    %% Get local observations
+    [reference_goal, observation_data, intermediate_goal] = generateData(mavPose, t, mavT, mavPath, map, params);
+    
+    X = [X, [reference_goal, observation_data]'];
+    T = [T, intermediate_goal'];
+    
     
     if params.visualization=='summary'
         show(map); hold on;
         plot(mavPath(:, 1), mavPath(:, 2), 'b'); hold on;
         plot(mavPose(1), mavPose(2), 'xr'); hold on;
         plot(global_start(1), global_start(2), 'xb'); hold on;
+        plot(reference_goal(1), reference_goal(2), 'ob'); hold on;
         plot(global_goal(1), global_goal(2), 'xg'); hold on;
         hold off;
         drawnow
---
+++ generateData.m
@@ -0,0 +1,36 @@
+function [reference_goal, observation_data, global_goal] = generateData(pose, time, time_path, path, map, param)
+
+map = robotics.OccupancyGrid(double(map.occupancyMatrix), param.globalmap.resolution);
+
+observation_data = getLaserScan(map, pose, param);
+reference_goal = getIntermediateGoal(pose, time, time_path, path);
+
+%% TODO: Convert to robocentric frame
+global_goal = path(end, :) - pose(1:2);
+
+%% 
+
+end
+
+function scandata = getLaserScan(map, pose, param)
+    scan_resolution = 1;
+    angles = -param.sensor.fov/2:scan_resolution:param.sensor.fov/2;
+    intsectionPts = rayIntersection(map, pose, angles, param.sensor.maxrange); % Generate rays
+    nan_mask = isnan(intsectionPts(:,1));
+    intsectionPts(nan_mask, :) = param.sensor.maxrange*ones(sum(nan_mask(:, 1)),2);
+    
+    scandata = vecnorm(intsectionPts - pose(1:2), 2, 2)';
+    
+end
+
+function goal = getIntermediateGoal(pose, time, time_path, path)
+    idx = sum(time_path <= time);
+    
+    for i = idx:1:size(time_path)
+        
+        if norm(path(idx, :) - pose(1:2)) > 1
+            break;
+        end
+    end
+    goal = path(idx, :);
+end
