6e9f4098b5cf8d1565d5b3137a8690540be4b665
JaeyoungLim <gokoreas@gmail.com>
2018-11-07 19:22:11 +0100

    Switch test process

--- navigator.m
+++ navigator.m
@@ -24,23 +24,24 @@ S = zeros(num_tests, num_trials); % Success Mask
 parameterfile.start_point = [5.0, 5.0]; 
 parameterfile.goal_point = [15.0, 15.0]; 
 
-%% Start Navigation
-for j=1:num_tests
-    % Switch test cases
-    parameterfile.global_planner = Test_planner{j};
-    parameterfile.localgoal = Test_goalselection{j};
-    
-    %% Generate Map only once
-    if parameterfile.map_generate
-        map = generate_envwithPos(parameterfile, parameterfile.start_point, parameterfile.goal_point);
-        save map;
-        parameterfile.map_generate = false;
-    else
-        map = load('map.mat', 'map');
-        map = map.map;
-    end
-    %% Navigate through environment and store results
-    for i = 1:num_trials
+%% Navigate through environment and store results
+for i = 1:num_trials
+    for j=1:num_tests
+        % Switch test cases
+        parameterfile.global_planner = Test_planner{j};
+        parameterfile.localgoal = Test_goalselection{j};
+
+        %% Generate Map only once
+        if parameterfile.map_generate
+            map = generate_envwithPos(parameterfile, parameterfile.start_point, parameterfile.goal_point);
+            save map;
+            parameterfile.map_generate = false;
+        else
+            map = load('map.mat', 'map');
+            map = map.map;
+        end
+        %% Start Navigation
+
         [time, path, failure] = navigate(parameterfile, map);
         S(j, i) = ~failure;
         if ~failure
@@ -48,6 +49,6 @@ for j=1:num_tests
             D(j, i) = distance_traveled;
             T(j, i) = time;
         end
-
     end
+    parameterfile.map_generate = true;
 end
