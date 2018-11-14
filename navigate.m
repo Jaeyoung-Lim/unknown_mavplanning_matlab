d9206219bd989015b4110d71ab1eb01d0583e498
JaeyoungLim <gokoreas@gmail.com>
2018-11-14 19:24:57 +0100

    store mav states in a struct

--- navigate.m
+++ navigate.m
@@ -1,7 +1,7 @@
-function [T, mavPath, failure] = navigate(params, binmap_true)
+function [T, mavpath, failure] = navigate(params, binmap_true)
 %% Initialize Parameters
 dt = 0.1;
-mavPath = [];
+initialize();
 T = 0;
 failure = false;
 local_goal_vel = [0.0, 0.0];
@@ -11,15 +11,15 @@ global_start = params.start_point; % Set gloabl start and goal position
 global_goal = params.goal_point;
 % init_yaw = atan2(global_goal(2)-global_start(2), global_goal(1)-global_start(1));
 init_yaw = pi()/2;
-mavPose = [global_start, init_yaw]; % Current position starts from global start
-prev_mavPose = mavPose;
-mavVel = [0.0, 0.0];
+mav.pose = [global_start, init_yaw]; % Current position starts from global start
+prev_mavPose = mav.pose;
+mav.velocity = [0.0, 0.0];
 
 % Initialize Local map
 localmap_obs = robotics.OccupancyGrid(params.globalmap.width, params.globalmap.height, params.globalmap.resolution);
 
-map_partial = get_localmap('increment', binmap_true, localmap_obs, params, mavPose); % 
-opt_binmap = get_optimisticmap(map_partial, params, mavPose); % Optimistic binary occupancy grid
+map_partial = get_localmap('increment', binmap_true, localmap_obs, params, mav.pose); % 
+opt_binmap = get_optimisticmap(map_partial, params, mav.pose); % Optimistic binary occupancy grid
 
 % Plan global trajectory
 switch params.global_planner
@@ -37,19 +37,19 @@ switch params.global_planner
 end
 
 %% Local replanning from global path
-[localmap_obs, ~] = get_localmap('increment', binmap_true, localmap_obs, params, mavPose);     % Create a partial map based on observation
+[localmap_obs, ~] = get_localmap('increment', binmap_true, localmap_obs, params, mav.pose);     % Create a partial map based on observation
 
 while true        
     % Replan Local trajectory from trajectory replanning rate
 
-    local_start = mavPose(1:2);
-    cons_binmap = get_conservativemap(localmap_obs, params, mavPose);
-    [local_goal, local_goal_yaw] = getLocalGoal(params, cons_binmap, mavPose, globalpath, global_goal, localmap_obs); % Parse intermediate goal from global path
+    local_start = mav.pose(1:2);
+    cons_binmap = get_conservativemap(localmap_obs, params, mav.pose);
+    [local_goal, local_goal_yaw] = getLocalGoal(params, cons_binmap, mav.pose, globalpath, global_goal, localmap_obs); % Parse intermediate goal from global path
     if ~isempty(local_goal_yaw)
         local_goal_vel = 0.0 * [cos(local_goal_yaw), sin(local_goal_yaw)];
-        [localT, localpath] = plan_trajectory('chomp', cons_binmap, local_start, local_goal, mavVel, local_goal_vel);
+        [localT, localpath] = plan_trajectory('chomp', cons_binmap, local_start, local_goal, mav.velocity, local_goal_vel);
     else
-        [localT, localpath] = plan_trajectory('chomp', cons_binmap, local_start, local_goal, mavVel);
+        [localT, localpath] = plan_trajectory('chomp', cons_binmap, local_start, local_goal, mav.velocity);
     end
     if detectLocalOptima(localpath)
         switch params.globalreplan
@@ -58,35 +58,36 @@ while true
                 disp('Stuck in local goal!');
                 break;
             case true
-                opt_binmap = get_optimisticmap(map_partial, params, mavPose); % Optimistic binary occupancy grid
+                opt_binmap = get_optimisticmap(map_partial, params, mav.pose); % Optimistic binary occupancy grid
                 % Global plan based on optimistic map
                 [~, globalpath] = plan_trajectory('polynomial', opt_binmap, local_start, global_goal);
         end
     end
     %% Move along local trajectory
     for t = 1:dt:params.update_rate
-        prev_mavPose = mavPose;
-        [mavPose, ~] = posefromtrajectoy(localpath, localT, t);
-        mavVel = mavPose(1:2) - prev_mavPose(1:2);
+        prev_mavPose = mav.pose;
+        [mav.pose, ~] = posefromtrajectoy(localpath, localT, t);
+        mav.velocity = mav.pose(1:2) - prev_mavPose(1:2);
         %TODO: Collision Checking
-        mavPath = [mavPath; mavPose(1:2)]; % Record trajectory
+        mav.path = [mav.path; mav.pose(1:2)]; % Record trajectory
         T = T + dt;        
 
-        [localmap_obs, ~] = get_localmap('increment', binmap_true, localmap_obs, params, mavPose);     % Create a partial map based on observation
+        [localmap_obs, ~] = get_localmap('increment', binmap_true, localmap_obs, params, mav.pose);     % Create a partial map based on observation
 
         if params.visualization
-            plot_summary(params, T, binmap_true, localmap_obs, mavPath, localpath, globalpath, mavPose, mavVel, local_goal_vel); % Plot MAV moving in environment
+            plot_summary(params, T, binmap_true, localmap_obs, localpath, globalpath, mav, local_goal_vel); % Plot MAV moving in environment
         end
         
-        if goalreached(mavPose(1:2), global_goal)
+        if goalreached(mav.pose(1:2), global_goal)
             break;
         end
     end
 
-    if goalreached(mavPose(1:2), global_goal)
+    if goalreached(mav.pose(1:2), global_goal)
         disp('Goal Reached!');
         break;
     end
 end
 
+mavpath = mav.path;
 end
