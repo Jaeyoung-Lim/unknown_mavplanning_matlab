cd666470b9e9746611aab94dd3674d5f5fc4bfe7
JaeyoungLim <gokoreas@gmail.com>
2018-11-25 20:43:55 +0100

    Vectorize linear kernel

--- learning/kernelFeatures.m
+++ learning/kernelFeatures.m
@@ -33,12 +33,13 @@ function k = kSparse(x, x_hat)
 end
 
 function k = kThreshold(x, x_hat)
-    k= zeros(size(x_hat, 1), 1); 
-    rth = 0.5;
+    k= zeros(size(x_hat, 1), 1, size(x, 1)); 
+    rth = 2;
+    x = reshape(x', [1, 2, size(x, 1)]);
     r = vecnorm(x-x_hat, 2, 2);
     
     thr_mask = r < rth;
     
     k(thr_mask) = (rth - r(thr_mask)) / rth;
-        
+    k = squeeze(k);    
 end
