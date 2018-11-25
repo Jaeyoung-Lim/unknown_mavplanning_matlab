9e8a143737f1aef5cfd72ef7d079ec3e80a77200
JaeyoungLim <gokoreas@gmail.com>
2018-11-25 16:09:42 +0100

    Failed attempt for vectorization

--- learning/grad_negativell.m
+++ learning/grad_negativell.m
@@ -1,10 +1,14 @@
 function grad_NLL = grad_negativell(param, w, X, Y, map)
 
+
+phi_x = kernelFeatures(param, X, map, param.hilbertmap.kernel);
 grad_NLL = zeros(size(w));
-
-
-    phi_x = kernelFeatures(param, X, map, param.hilbertmap.kernel);
 for i = 1:size(Y, 1) % TODO: vectorize this calculation
     grad_NLL = grad_NLL -Y(i)* phi_x(:, i) / ( 1 + exp(Y(i)*dot(w, phi_x(:, i))));
 end
+
+% Vectorized above calculation, but slower :(
+% grad_NLL_stretch =  phi_x * diag((Y ./ (1+exp(diag(Y)*(w'*phi_x)'))));
+% grad_NLL =  -1.0 * sum(grad_NLL_stretch, 2);
+
 end
