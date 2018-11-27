b57320af3609d827654cd9dab154fd072c0c3dd3
JaeyoungLim <gokoreas@gmail.com>
2018-11-27 16:26:05 +0100

    Benchmarks for kernel calculation

--- learning/test_hilbertmap.m
+++ learning/test_hilbertmap.m
@@ -28,46 +28,74 @@ map = create_random_map(4, 4, 10, 10, 0.4);
 
 %% Benchmark on number of achor points
 
+% resolution = 1:5:80;
+% num_trials = 50;
+% calc_time = zeros(size(resolution, 2), num_trials);
+% num_anchorpoints = zeros(size(resolution));
+% for j = 1:num_trials
+%     for i = 1:size(resolution, 2)
+%         params.hilbertmap.resolution = resolution(i);
+%         X = rand(num_samples, 1) * 2 + 1;
+%         Y = rand(num_samples, 1) * 2 + 1;
+%         xy = [X(:), Y(:)];
+% 
+%         y = double(map.getOccupancy(xy));
+%         zero_mask = y < 1;
+%         y(zero_mask) = -1;
+%         [wt, time] = learn_hilbert_map(params, map, xy, y);
+%         calc_time(i, j) = time;
+%         num_anchorpoints(i) = size(wt, 1);
+%     end
+% end
+% figure(1);
+% mean_calc_time = mean(calc_time, 2);
+% stdev_calc_time = std(calc_time, 0, 2);
+% errorbar(num_anchorpoints, mean_calc_time, stdev_calc_time, 'ob-');
+% title('Regression time vs number of anchor points');
+% xlabel('Number of Anchor Points'); ylabel('Training Time [s]');
+
+%% Benchmark on number of samples
+% num_samples_set = 1:20:500;
+% num_trials = 50;
+% calc_time = zeros(size(num_samples_set, 2), num_trials);
+% for j = 1:num_trials
+%     for i = 1:size(num_samples_set, 2)
+%         num_samples = num_samples_set(i);
+%         X = rand(num_samples, 1) * 2 + 1;
+%         Y = rand(num_samples, 1) * 2 + 1;
+%         xy = [X(:), Y(:)];
+% 
+%         y = double(map.getOccupancy(xy));
+%         zero_mask = y < 1;
+%         y(zero_mask) = -1;
+%         [wt, time] = learn_hilbert_map(params, map, xy, y);
+%         calc_time(i, j) = time;
+% 
+%     end
+% end
+% mean_calc_time = mean(calc_time, 2);
+% stdev_calc_time = std(calc_time, 0, 2);
+% 
+% errorbar(num_samples_set', mean_calc_time, stdev_calc_time, 'ob-');
+% title('Regression time vs number of samples');
+% xlabel('Number of Samples'); ylabel('Training Time [s]');
+
+%% Benchmark on Kernel Calculation
 resolution = 1:5:80;
-calc_time = zeros(size(resolution));
+calc_time = zeros(size(resolution, 2), 1);
 num_anchorpoints = zeros(size(resolution));
 for i = 1:size(resolution, 2)
     params.hilbertmap.resolution = resolution(i);
     X = rand(num_samples, 1) * 2 + 1;
     Y = rand(num_samples, 1) * 2 + 1;
     xy = [X(:), Y(:)];
-
-    y = double(map.getOccupancy(xy));
-    zero_mask = y < 1;
-    y(zero_mask) = -1;
-    [wt, time] = learn_hilbert_map(params, map, xy, y);
-    calc_time(i) = time;
-    num_anchorpoints(i) = size(wt, 1);
+    tic;
+    phi_x = kernelFeatures(params, xy, map, params.hilbertmap.kernel);
+    calc_time(i) = toc; 
+    num_anchorpoints(i) = size(phi_x, 1);
 end
+
 figure(1);
 plot(num_anchorpoints, calc_time, 'ob-');
-title('Regression time vs number of anchor points');
+title('kernel calc time vs number of anchor points');
 xlabel('Number of Anchor Points'); ylabel('Training Time [s]');
-
-%% Bench mark on number of samples
-
-% num_samples_set = 1:20:500;
-% calc_time = zeros(size(num_samples_set));
-% 
-% for i = 1:size(num_samples_set, 2)
-%     num_samples = num_samples_set(i);
-%     X = rand(num_samples, 1) * 2 + 1;
-%     Y = rand(num_samples, 1) * 2 + 1;
-%     xy = [X(:), Y(:)];
-% 
-%     y = double(map.getOccupancy(xy));
-%     zero_mask = y < 1;
-%     y(zero_mask) = -1;
-%     [wt, time] = learn_hilbert_map(params, map, xy, y);
-%     calc_time(i) = time;
-% 
-% end
-% 
-% plot(num_samples_set, calc_time, 'ob-');
-% title('Regression time vs number of samples');
-% xlabel('Number of Samples'); ylabel('Training Time [s]');
