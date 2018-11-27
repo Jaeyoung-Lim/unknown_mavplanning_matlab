aaa8fbd11406fdb02de830a65a6604775a1c55c4
JaeyoungLim <gokoreas@gmail.com>
2018-11-27 20:23:41 +0100

    This is a hack for local maps with local goal

--- trajectory/getLocalGoal.m
+++ trajectory/getLocalGoal.m
@@ -4,28 +4,31 @@ if nargin < 6
     occupancy_map = [];
 end
 
-if ~binoccupancy_map.getOccupancy(goal)
-    % Local goal is global goal if global goal is free
-    local_goal = goal;
-    local_goal_vel = zeros(1,2);
-else
-    switch param.localgoal
-        case 'frompath'
-            %% Pick Goal from Path
-            local_goal = goalfrompath(binoccupancy_map, path, curr_pose(1:2), param.plan_horizon);
+% if ~binoccupancy_map.getOccupancy(goal)
+%     % Local goal is global goal if global goal is free
+%     local_goal = goal;
+%     local_goal_vel = zeros(1,2);
+% else
+%     switch param.localgoal
+%         case 'frompath'
+%             %% Pick Goal from Path
+%             local_goal = goalfrompath(binoccupancy_map, path, curr_pose(1:2), param.plan_horizon);
+% 
+%         case 'random'
+%             %% Pick Random Goal
+%             local_goal = samplePosfromMap(binoccupancy_map);
+%             
+%         case {'nextbestview', 'nextbestview-yaw'}
+%             %% Pick Next Best View
+%             [local_goal, local_goal_vel] = getNextBestView(param, binoccupancy_map, curr_pose, goal, occupancy_map);
+%         otherwise
+%             disp('invalid local planner parameter')
+% 
+%     end
+% end
 
-        case 'random'
-            %% Pick Random Goal
-            local_goal = samplePosfromMap(binoccupancy_map);
-            
-        case {'nextbestview', 'nextbestview-yaw'}
-            %% Pick Next Best View
-            [local_goal, local_goal_vel] = getNextBestView(param, binoccupancy_map, curr_pose, goal, occupancy_map);
-        otherwise
-            disp('invalid local planner parameter')
-
-    end
-end
+local_goal = goal;
+local_goal_vel = zeros(1,2);
 
 
 end
