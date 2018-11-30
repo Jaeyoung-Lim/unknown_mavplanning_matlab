0ee021a9941d4588b682ac431b545a8f83e1bef7
JaeyoungLim <gokoreas@gmail.com>
2018-11-30 14:14:53 +0100

    Add regularizer for l2 norm

--- learning/grad_negativell.m
+++ learning/grad_negativell.m
@@ -6,6 +6,12 @@ grad_NLL = zeros(size(w));
 for i = 1:size(Y, 1) % TODO: vectorize this calculation
     grad_NLL = grad_NLL -Y(i)* phi_x(:, i) / ( 1 + exp(Y(i)*dot(w, phi_x(:, i))));
 end
+if ~(norm(w, 2) == 0)
+    regularize = w / norm(w, 2);
+else
+    regularize = zeros(size(w));
+end
+grad_NLL = grad_NLL + regularize;
 
 % Vectorized above calculation, but slower :(
 % grad_NLL_stretch =  phi_x * diag((Y ./ (1+exp(diag(Y)*(w'*phi_x)'))));
