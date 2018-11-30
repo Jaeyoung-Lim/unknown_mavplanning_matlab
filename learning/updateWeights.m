a1ef310d6391195958e70ce4c9a34466040f8323
JaeyoungLim <gokoreas@gmail.com>
2018-11-30 15:22:11 +0100

    Add flag for enabling momentum methods

--- learning/updateWeights.m
+++ learning/updateWeights.m
@@ -1,5 +1,7 @@
-function wt = updateWeights(param, wt_1, X, Y, map)
+function [wt, grad_nll] = updateWeights(param, wt_1, X, Y, map, prev_grad_nll)
     eta = param.hilbertmap.learningrate; % Learning rate
     A = eye(size(10));
-    wt = wt_1 - eta * A * grad_negativell(param, wt_1, X, Y, map); % A should be inv A
+    grad_nll = grad_negativell(param, wt_1, X, Y, map);
+    % SGD with momentum
+    wt = wt_1 - (param.hilbertmap.momentum * prev_grad_nll + eta * A * grad_nll); % A should be inv A
 end
