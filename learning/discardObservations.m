fd7c18fad23cab1e61856894413ab058e03d618b
JaeyoungLim <gokoreas@gmail.com>
2018-11-30 23:40:07 +0100

    get rid of debug outputs

--- learning/discardObservations.m
+++ learning/discardObservations.m
@@ -1,5 +1,5 @@
 function [xy, y] = discardObservations(param, xy, y, pose)
-    size(xy, 1)
+
     map_mask = vecnorm(xy - pose(1:2), 2, 2) < param.localmap.width;
     xy = xy(map_mask, :);
     y = y(map_mask, :);
