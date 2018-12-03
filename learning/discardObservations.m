b64d173d2dcd2ac0cfe3de4c867b93601ca8adf9
JaeyoungLim <gokoreas@gmail.com>
2018-12-03 09:21:31 +0100

    Get local goal randomly if it is outside the map

--- learning/discardObservations.m
+++ learning/discardObservations.m
@@ -3,6 +3,5 @@ function [xy, y] = discardObservations(param, xy, y, pose)
     map_mask = vecnorm(xy - pose(1:2), 2, 2) < param.localmap.width;
     xy = xy(map_mask, :);
     y = y(map_mask, :);
-    size(xy, 1)
 
 end
--- trajectory/getLocalGoal.m
+++ trajectory/getLocalGoal.m
@@ -12,28 +12,32 @@ switch param.mapping
         
 end
 
+if isinsideMap(param, binoccupancy_map, goal)
+    if ~binoccupancy_map.getOccupancy(goal)
+        % Local goal is global goal if global goal is free
+        local_goal = goal;
+        local_goal_vel = zeros(1,2);
+    else
+        switch param.localgoal
+            case 'frompath'
+                %% Pick Goal from Path
+                local_goal = goalfrompath(binoccupancy_map, path, curr_pose(1:2), param.plan_horizon);
 
-if ~binoccupancy_map.getOccupancy(goal) && isinsideMap(param, binoccupancy_map, goal)
-    % Local goal is global goal if global goal is free
-    local_goal = goal;
-    local_goal_vel = zeros(1,2);
-else
-    switch param.localgoal
-        case 'frompath'
-            %% Pick Goal from Path
-            local_goal = goalfrompath(binoccupancy_map, path, curr_pose(1:2), param.plan_horizon);
+            case 'random'
+                %% Pick Random Goal
+                local_goal = samplePosfromMap(binoccupancy_map);
 
-        case 'random'
-            %% Pick Random Goal
-            local_goal = samplePosfromMap(binoccupancy_map);
-            
-        case {'nextbestview', 'nextbestview-yaw'}
-            %% Pick Next Best View
-            [local_goal, local_goal_vel] = getNextBestView(param, binoccupancy_map, curr_pose, goal, occupancy_map);
-        otherwise
-            disp('invalid local planner parameter')
+            case {'nextbestview', 'nextbestview-yaw'}
+                %% Pick Next Best View
+                [local_goal, local_goal_vel] = getNextBestView(param, binoccupancy_map, curr_pose, goal, occupancy_map);
+            otherwise
+                disp('invalid local planner parameter')
 
+        end
     end
+else
+    local_goal = samplePosfromMap(binoccupancy_map);
+    local_goal_vel = zeros(1,2);    
 end
 
 switch param.mapping
