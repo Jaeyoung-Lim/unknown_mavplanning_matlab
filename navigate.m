752a23fb74ed91b871161470d654df8ddc1269d7
JaeyoungLim <gokoreas@gmail.com>
2018-12-03 22:32:21 +0100

    Export functions

--- navigate.m
+++ navigate.m
@@ -117,8 +117,20 @@ end
 mavpath = mav.path;
 end
 
-function collision = isCollision(pos, map)
-    collision = getOccupancy(map, pos);
+function globalpath = planGlobalTrajectory(params, binmap_true, global_start, global_goal, map_partial)
+    % Plan global trajectory
+    switch params.global_planner
+        case 'optimistic'
+            % Global plan based on optimistic map
+            opt_binmap = get_optimisticmap(map_partial, params, mav.pose); % Optimistic binary occupancy grid
+            [~, globalpath] = plan_trajectory('polynomial', opt_binmap, global_start, global_goal);
 
+        case 'true'
+            % Global plan based on true map
+            [~, globalpath] = plan_trajectory('polynomial', binmap_true, global_start, global_goal);
+        case 'disable'
+            % Disable global planner
+            globalpath = [];
+
+    end
 end
-
