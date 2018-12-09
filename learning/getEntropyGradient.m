3f17684673db13096cbb294f4bf89664e337d83f
JaeyoungLim <gokoreas@gmail.com>
2018-12-09 23:33:42 +0100

    Handle exception when weights are not defined

--- learning/getEntropyGradient.m
+++ learning/getEntropyGradient.m
@@ -1,6 +1,11 @@
 function [dH, H] = getEntropyGradient(param, x_query, map, hilbertmap)
     epsilon = 1e-4;
-    wt = hilbertmap.wt;
+    if isempty(hilbertmap.wt)
+        num_features = param.hilbertmap.resolution^2 * map.XWorldLimits(2) * map.YWorldLimits(2);
+        wt = zeros(num_features, 1);
+    else
+        wt = hilbertmap.wt;        
+    end
     p = occupancyProb(param, hilbertmap.wt, x_query, map);
     % Calculate entropy
     H = - p*log2(p) - (1-p) * log2(1 - p);
