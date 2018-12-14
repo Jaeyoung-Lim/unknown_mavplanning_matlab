a3c7aae70ca0a8766599265f3c9d364c3f255a2b
JaeyoungLim <gokoreas@gmail.com>
2018-12-14 12:01:40 +0100

    Handle exceptions for no additional observations

--- learning/sampleObservations.m
+++ learning/sampleObservations.m
@@ -1,9 +1,13 @@
 function [xy, y] = sampleObservations(free_space, occupied_space, xy, y)
-    freespace = free_space(randi([1 size(free_space, 1)], min(5, size(free_space, 1)), 1), :);
-    occupiedspace = occupied_space(randi([1 size(occupied_space, 1)], min(3, size(occupied_space, 1)), 1), :);
-    xy = [xy; occupiedspace];
-    y = [y; ones(size(occupiedspace, 1), 1)];
-    xy = [xy; freespace];
-    y = [y; -1 * ones(size(freespace, 1), 1)];
+    if ~isempty(free_space)
+        freespace = free_space(randi([1 size(free_space, 1)], min(5, size(free_space, 1)), 1), :);
+        xy = [xy; freespace];
+        y = [y; -1 * ones(size(freespace, 1), 1)];
+    end
+    if ~isempty(occupied_space)
+        occupiedspace = occupied_space(randi([1 size(occupied_space, 1)], min(3, size(occupied_space, 1)), 1), :);
+            xy = [xy; occupiedspace];
+        y = [y; ones(size(occupiedspace, 1), 1)];
+    end
 
 end
