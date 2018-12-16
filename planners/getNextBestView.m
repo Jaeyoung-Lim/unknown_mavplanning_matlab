66710c7c45fad772d6030a7d8a643e4fb0d89cf2
JaeyoungLim <gokoreas@gmail.com>
2018-12-16 11:41:06 +0100

    Add new nextbestview using information gradient

--- planners/getNextBestView.m
+++ planners/getNextBestView.m
@@ -2,8 +2,6 @@ function [goal, goal_vel] = getNextBestView(param, binmap, pose, global_goal, ma
     %% Next best view planner from Oleynikova 2017
     % Parameters
     num_samples = 80;
-    we = 0.01;
-    wg = 0.4;
     r = 0.5;
     vel = 3.0;
 
@@ -15,44 +13,61 @@ function [goal, goal_vel] = getNextBestView(param, binmap, pose, global_goal, ma
     yaw = zeros(num_samples, 1);
     sample_yaw = 0.0;
     
-    for i=1:num_samples
-        sample_pos = samplePosfromMap(binmap);
-        pos(i, :) = sample_pos;
-        switch param.localgoal
-            case 'nextbestview'
-                sample_yaw = atan2(sample_pos(2) - mav_pos(2), sample_pos(1) - mav_pos(1)); % From Oleynikova 2017
-                vel = 0.0;
-            case {'nextbestview-yaw', 'nextbestview-hilbert'}
-                sample_yaw = 2*pi()*rand();
-                yaw(i) = sample_yaw;
-                vel = 1.0;
-        end
-        sample_pose = [sample_pos, sample_yaw];
-        l(i) = getExplorationgain(param, map, sample_pose);
-        dg = norm(global_goal-mav_pos) + r;
-        R(i) = we * l(i) + wg * (dg  - norm(global_goal - sample_pos))/dg;
-    end
-    [~, idx] = max(R);
-    goal = pos(idx, :);
-    goal_vel = vel * [cos(yaw(idx)), sin(yaw(idx))];
+    goal = global_goal;
+    goal_vel = vel*[cos(pose(3)), sin(pose(3))];
     
     switch param.localgoal
+        case {'nextbestview', 'nextbestview-yaw'}
+            for i=1:num_samples
+                sample_pos = samplePosfromMap(binmap); % Sample from conservative binary occupancy map
+                pos(i, :) = sample_pos;
+                switch param.localgoal
+                    case 'nextbestview'
+                        we = 0.01;
+                        wg = 1;
+                        sample_yaw = atan2(sample_pos(2) - mav_pos(2), sample_pos(1) - mav_pos(1)); % From Oleynikova 2017
+                        vel = 0.0;
+                        sample_pose = [sample_pos, sample_yaw];
+                    case 'nextbestview-yaw'
+                        we = 0.01;
+                        wg = 0.4;
+                        sample_yaw = 2*pi()*rand();
+                        yaw(i) = sample_yaw;
+                        vel = 1.0;
+                        sample_pose = [sample_pos, sample_yaw];
+                end
+                
+                l(i) = getExplorationgain(param, map, sample_pose);
+                dg = norm(global_goal-mav_pos) + r;
+                R(i) = we * l(i) + wg * (dg  - norm(global_goal - sample_pos))/dg;
+                [~, idx] = max(R);
+                goal = pos(idx, :);
+                goal_vel = vel * [cos(yaw(idx)), sin(yaw(idx))];
+            end
         case 'nextbestview-hilbert'
-            while true
-                [goal, goal_vel, omega] = updateLocalGoal(param, map, goal, goal_vel, hilbertmap);
-                if abs(omega) < 0.03
+            we = 1;
+            wg = 0.4;
+            vel = 1.0;
+            % Get a intermediate goal point
+            goal = mav_pos + 0.5*param.sensor.maxrange * [cos(pose(3)), sin(pose(3))];
+            
+            while true % Update intermediate goal point with gradients
+                [dl, l] = getMIGradient(param, map, goal, goal_vel, hilbertmap); %Information cost
+                [dg, g] = getGoalGradient(goal); % Goal directed cost
+                goal = goal+100*dl;
+                R = we * l + wg * g;
+                dR = we * dl + wg * dg;
+                                
+                fprintf('goalcost: %d infcost: %d ratio: %d\n', l, g, l/g);
+                if norm(dR) <= 1e-4
                     break;
                 end
             end
     end
 end
 
-function [goal, goal_vel, omega] = updateLocalGoal(param, map, goal, goal_vel, hilbertmap)
-    dl = getMIGradient(param, map, goal, goal_vel, hilbertmap);
-    curr_yaw = atan2(goal_vel(2), goal_vel(1));
-    yaw_vec = cross([goal_vel, 0], [dl, 0]);
-    omega = 10*yaw_vec(3);
-    goal = goal;
-    goal_vel = norm(goal_vel) * [cos(curr_yaw + omega), sin(curr_yaw+omega)];
-    
+function [dg, g] = getGoalGradient(goal)
+    goal_dist = norm(global_goal-mav_pos) + r;
+    g  = (goal_dist  - norm(global_goal - goal))/goal_dist;
+
 end
