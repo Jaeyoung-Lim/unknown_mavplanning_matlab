def5ba03dee42476d77cdd0d691458872b85de5b
JaeyoungLim <gokoreas@gmail.com>
2019-01-07 11:35:48 +0100

    Add radial anchorpoint patterns

--- learning/kernelFeatures.m
+++ learning/kernelFeatures.m
@@ -3,11 +3,32 @@ function phi_hat = kernelFeatures(param, x_query, map, option)
     % X_data : Sample points
     % Create data from binary occupancy points
     res = 1/(param.hilbertmap.resolution);
-    switch param.mapping
-        case 'local'
-            [X, Y] = meshgrid((-0.5*map.XWorldLimits(2) + res):res:0.5*map.XWorldLimits(2), (- 0.5 * map.YWorldLimits(2)+ res):res:0.5 * map.YWorldLimits(2));
-        otherwise
-            [X, Y] = meshgrid(res:res:map.XWorldLimits(2), res:res:map.YWorldLimits(2));
+    % Create anchorpoints
+    switch param.hilbertmap.pattern
+        case 'grid'
+            switch param.mapping
+                case 'local'
+                    [X, Y] = meshgrid((-0.5*map.XWorldLimits(2) + res):res:0.5*map.XWorldLimits(2), (- 0.5 * map.YWorldLimits(2)+ res):res:0.5 * map.YWorldLimits(2));
+                otherwise
+                    [X, Y] = meshgrid(res:res:map.XWorldLimits(2), res:res:map.YWorldLimits(2));
+            end
+        case 'radial'
+            switch param.mapping
+                case 'local'
+                    r = res:res:map.XWorldLimits(2);
+                    ang_res = 2*pi() * res / map.XWorldLimits(2);
+                    theta = ang_res:ang_res:2*pi();
+                    origin = [0, 0];
+                    [X, Y] = meshgrid(r.*cos(theta) + origin(1), r.*sin(theta) + origin(2));
+
+                otherwise
+                    r = res:res:map.XWorldLimits(2);
+                    ang_res = 2*pi() * res / map.XWorldLimits(2);
+                    theta = ang_res:ang_res:2*pi();
+                    origin = [0.5*map.XWorldLimits(2), 0.5*map.YWorldLimits(2)];
+                    [X, Y] = meshgrid(r.*cos(theta) + origin(1), r.*sin(theta) + origin(2));
+                    
+            end            
     end
     X = X(:);
     Y= Y(:);
