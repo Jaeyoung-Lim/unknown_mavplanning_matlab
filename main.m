19ac8203c21b8446970084dd545f46109ec120f9
Jaeyoung-Lim <jalim@ethz.ch>
2021-01-27 15:25:04 +0100

    Update
    Fix
    
    
    Fix

--- main.m
+++ main.m
@@ -9,23 +9,12 @@
 clc; clear all; close all;
 
 % Parameters
-% parameterfile = Param_TINYRANDOMFOREST;
-% parameterfile = Param_RANDOMFOREST;
 parameterfile = Param_LOCALRANDOMFOREST;
-% parameterfile = Param_CORRIDOR;
-% parameterfile = Param_LOCALCORRIDOR;
-% parameterfile = Param_CORNER;
-% parameterfile = Param_LOCALCORNER
 
 num_trials = 2; % Number of trials for statistics
 Test_planner = {'hilbertchomp', 'chomp'}; % Configuration for different test sets
 Test_map = {'local', 'increment'};
 Test_goalselection = {'frompath', 'nextbestview'};
-
-% Test_planner = {'chomp'}; % Configuration for different test sets
-% Test_map = {'increment'};
-% Test_goalselection = {'nextbestview'};
-
 
 %% Initialize variables for statistics
 num_tests = size(Test_planner, 2);
@@ -44,6 +33,7 @@ for i = 1:num_trials
 
         %% Generate Map only once
         if parameterfile.map_generate
+            %TODO: No need to generate environment maps
             [map, start_pos, goal_pos] = generate_envwithPos(parameterfile, parameterfile.start_point, parameterfile.goal_point);
             parameterfile.start_point = start_pos;
             parameterfile.goal_point = goal_pos;
--- planners/test_hilbertmap_inf_soaring.m
+++ planners/test_hilbertmap_inf_soaring.m
@@ -1,37 +1,39 @@
+clc; close all;
 %% Create a random map.
-close all;
 map_size = 1;
 
 param = Param_TINYRANDOMFOREST;
+figure('name', 'Navigator', 'NumberTitle', 'off', 'Position', [100 800 400 400]);
+figure('name', 'Optimizer', 'NumberTitle', 'off', 'Position', [1900 800 400 400]);
 
 
-[map, start_pos, goal_pos] = generate_envwithPos(param, param.start_point, param.goal_point);
+occupancymap = struct('localmap', [], ... % Locally observed map
+                      'incrementmap', [], ... % Global observed map
+                      'truemap', []); % True binary occupancy map
+
+
+[occupancymap.truemap, start_pos, goal_pos] = generate_envwithPos(param, param.start_point, param.goal_point);
 start_point = start_pos;
 goal_point = goal_pos;
-
-% map = create_random_map(4, 4, 10, 10, 0.4);
-
-%   start_point = [0.5 0.5];
-%   goal_point = [3.5 3.5];
-% 
-% 
-% %% Clear 0.5, 0.5 and 3.5, 3.5
-% setOccupancy(map, vertcat(start_point, goal_point, ...
-%   start_point+0.05, goal_point+0.05, start_point-0.05, goal_point-0.05), 0);
+map = occupancymap.truemap;
 
 %% Get Hilbert map
-localmap_obs = initlocalmap(param);
-localmap_obs = get_localmap(param.mapping, localmap_obs, param, [start_point, pi()/2]); 
+occupancymap.incrementmap = initlocalmap(param);
+occupancymap.localmap = initlocalmap(param);
+
 hilbertmap.wt = [];
 hilbertmap.xy = [];
 hilbertmap.y = [];
 res = 0.5;
-num_samples = 81;
-% [X, Y] = meshgrid(res:res:(map.XWorldLimits(2)-res), res:res:(map.YWorldLimits(2)-res));
-% [X, Y] = meshgrid(res:res:0.8*(map.XWorldLimits(2)-res), res:res:0.8*(map.YWorldLimits(2)-res));
-X = rand(num_samples, 1) * 0.6*map.XWorldLimits(2);
-Y = rand(num_samples, 1) * 0.6*map.YWorldLimits(2);
+num_samples = 41;
+X = map.XWorldLimits(2) - rand(num_samples, 1) * 0.5 *map.XWorldLimits(2);
+Y = rand(num_samples, 1) * 1.0 *map.YWorldLimits(2);
 xy =  [X(:), Y(:)];
+num_samples = 21;
+X = rand(num_samples, 1) * 0.5 *map.XWorldLimits(2);
+Y = rand(num_samples, 1) * 0.5 *map.YWorldLimits(2);
+xy =  [xy; X(:), Y(:)];
+
 hilbertmap.xy = xy;
 
 y = double(map.getOccupancy(xy));
@@ -40,7 +42,7 @@ y(zero_mask) = -1;
 hilbertmap.y = y;
 
 
-hilbertmap = learn_hilbert_map(param, map, hilbertmap);
+hilbertmap = learn_hilbert_map(param, occupancymap, hilbertmap, [start_point, 0]);
 %% Get a straight line plan.
 %path = [start_point; goal_point];
 
@@ -58,7 +60,7 @@ trajectory = add_vertex_to_trajectory(trajectory, 3*(start_point+goal_point)/4, 
 
 %trajectory = add_vertex_to_trajectory(trajectory, 3*(start_point+goal_point)/4, 0, 1);
 
-trajectory = add_vertex_to_trajectory(trajectory, goal_point, 1, 0);
+trajectory = add_vertex_to_trajectory(trajectory, goal_point, 0, 1);
 
 % Estimate segment times.
 trajectory = estimate_trajectory_times(trajectory, v_max, a_max);
@@ -67,15 +69,15 @@ trajectory = solve_trajectory(trajectory);
 %trajectory = plan_path_waypoints([start_point;goal_point]);
 
 %% Optimize path around obstacles.
-% trajectory_opt = optimize_path_collisions(trajectory);
-% trajectory = optimize_trajectory_collisions(map, trajectory, 0);
+figure(findobj('name', 'Optimizer'));
+show(map); hold on;
 trajectory_chomp = optimize_trajectory_collisions_free(map, trajectory, 0);
-trajectory_hilbert = optimize_trajectory_collisions_hilbert(map, trajectory, 0, 0, 0.1, localmap_obs, hilbertmap, param);
-trajectory_hilbert_inf = optimize_trajectory_collisions_hilbert_inf(map, trajectory, 0, 0, 0.1, localmap_obs, hilbertmap, param);
+trajectory_hilbert = optimize_trajectory_collisions_hilbert(map, trajectory, 0, 0, 0.1, occupancymap.localmap, hilbertmap, param);
+
 
 %% Plot
 figure(1);
-% subplot(1, 3, 1);
+subplot(1, 2, 1);
 [cost_map, cost_map_x, cost_map_y] = get_cost_map(map);
 plot_map_table(map, cost_map);
 colorbar;
@@ -87,8 +89,8 @@ plot(p(:, 1), p(:, 2), 'w');
 
 hold off;
 
-figure(2);
-subplot(1, 2, 1);
+% figure(2);
+subplot(1, 2, 2);
 xy = hilbertmap.xy;
 y = hilbertmap.y;
 wt = hilbertmap.wt;
@@ -99,8 +101,14 @@ time = toc;
 fprintf('Render Time: %d\n',time)
 
 imagesc(map.XWorldLimits, fliplr(map.YWorldLimits), flipud(map_hilbert'));
+
 set(gca, 'Ydir', 'normal');
 hold on;
+
+if ~isempty(xy)
+    plot(xy((y > 0), 1), xy((y > 0), 2), 'xr'); hold on;
+    plot(xy((y < 0), 1), xy((y < 0), 2), 'xb'); hold on;
+end
 
 % colormap(gca, 'jet');
 colorbar('Ticks',[]);
@@ -110,18 +118,3 @@ axis image;
 plot([start_point(1), goal_point(1)], [start_point(2), goal_point(2)], 'xw');  hold on;
 [t, p1] = sample_trajectory(trajectory_hilbert, 0.1);
 plot(p1(:, 1), p1(:, 2), 'w');  hold on;
-
-subplot(1, 2, 2);
-imagesc(map.XWorldLimits, fliplr(map.YWorldLimits), flipud(map_hilbert'));
-set(gca, 'Ydir', 'normal');
-hold on;
-
-% colormap(gca, 'jet');
-colorbar('Ticks',[]);
-title('with Information Gain');
-xlabel('x position [m]'); ylabel('y position [m]');
-axis image;
-plot([start_point(1), goal_point(1)], [start_point(2), goal_point(2)], 'xw');  hold on;
-[t, p2] = sample_trajectory(trajectory_hilbert_inf, 0.1);
-plot(p2(:, 1), p2(:, 2), 'w');  hold on;
-
